9684e525cfa64e1293b42defb0218af74dd6fe2e
Frizaski Alfath <frizaskialfath28@gmail.com>
2024-11-04 17:07:45 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#0455BF',
+        'secondary': '#172554',
+      },
+    },
+  },
+  plugins: [],
+}
+
