b42fb5a731242ff5a4abe4ee3a10ac2e6254ff99
Frizaski Alfath <frizaskialfath28@gmail.com>
2024-11-06 21:58:32 +0700

    Menambahkan api untuk mengirim feedback ke email

--- src/script.js
+++ src/script.js
@@ -20,18 +20,25 @@ document.addEventListener('DOMContentLoaded', function() {
             `;
         }
     });
-    document.addEventListener('DOMContentLoaded', function() {
-        const form = document.querySelector('form'); // Pastikan ini adalah form yang benar
+    emailjs.init("dsuLOXyydWGukQ9sc");
 
-        form.addEventListener('submit', function(event) {
-            event.preventDefault(); // Mencegah form dari reload halaman
+    // Fungsi untuk mengirim email
+    function sendMail(event) {
+        event.preventDefault(); // Mencegah form dari reload halaman
 
-            emailjs.sendForm('service_2snthmg', 'template_bawnm05', this)
-              .then((result) => {
-                alert('Email berhasil dikirim!');
-              }, (error) => {
-                alert('Gagal mengirim email, coba lagi.');
-              });
-        });
-    });
+        emailjs.sendForm('service_2snthmg', 'template_bawnm05', '#contactForm')
+            .then(function(response) {
+                console.log('SUCCESS!', response.status, response.text);
+                alert('Pesan berhasil dikirim!');
+                
+                // Mengosongkan formulir
+                document.getElementById('contactForm').reset();
+            }, function(error) {
+                console.log('FAILED...', error);
+                alert('Pesan gagal dikirim, silakan coba lagi.');
+            });
+    }
+
+    // Pastikan form memanggil fungsi sendMail
+    document.getElementById('contactForm').onsubmit = sendMail;
 }); 
